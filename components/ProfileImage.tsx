b6156a9f4532109f88101e709d61297acde2689e
harshmuriki <harshsuhith@gmail.com>
2024-12-16 18:51:08 -0800

    Updated profile pic

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,18 +1,18 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const ProfileImage = (): JSX.Element => {
-    
-    return (
-        <Image
-            className="rounded-full select-none transition-all pointer-events-none"
-            src="/images/me.jpg"
-            // onMouseOver = {e => e.currentTarget.src = "/images/gelo.HEIC"}
-            // onMouseOut = {e => e.currentTarget.src = "/images/me.jpg"}
-            draggable={false}
-            alt="My profile image"
-            width={260}
-            height={260}/>
-    );
+  return (
+    <Image
+      className="rounded-full select-none transition-all pointer-events-none"
+      src="/images/me.jpeg"
+      // onMouseOver = {e => e.currentTarget.src = "/images/gelo.HEIC"}
+      // onMouseOut = {e => e.currentTarget.src = "/images/me.jpg"}
+      draggable={false}
+      alt="My profile image"
+      width={260}
+      height={260}
+    />
+  );
 };
 
 export default ProfileImage;
